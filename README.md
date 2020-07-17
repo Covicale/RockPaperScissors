02d28d8facc8b4ae7db091f2c095021a545c5ada
Alexander <ag.covic@gmail.com>
2020-07-17 17:23:35 +0100

    Rock Papers Scissors GUI

--- README.md
+++ README.md
@@ -6,8 +6,6 @@ Simple website simulating the game "Rock, Papers, Scissors"
 
 * [Website](https://covicale.github.io/RockPaperScissors/) - ROCK, PAPERS, SCISSORS!
 
-### WHY?
+### THANKS TO
 
-This project is part of the **"The Odin Project"**.
-
-If you want to see more about this, get in here: [TheOdinProject](https://www.theodinproject.com/)
+* [FlatIcon](https://www.flaticon.com/) -> Thanks to FLATICON for the free icons =)
--- script.js
+++ script.js
@@ -1,48 +1,120 @@
-function check(player1, player2){
-    try{
-        if (player1.toLowerCase() == 'rock' && player2.toLowerCase() == 'papers'){
-            return "Player 2 has won.";
-        }else if (player1.toLowerCase() == 'scissors' && player2.toLowerCase() == 'papers'){
-            return "Player 2 has won.";
-        }else if (player1.toLowerCase() == player2.toLowerCase()){
-            return "Draw.";
-        }else{
-            return "Player 1 has won.";
+
+window.onload = () => {
+    let gameStarted = false;
+
+    let options = document.getElementsByClassName("optBtn");
+    let playerChose = document.getElementById("playerIcon");
+    let pcChose = document.getElementById("pcIcon");
+    let resultadoTexto = document.getElementById("resText");
+
+    let playerPoints = 0;
+    let pcPoints = 0;
+
+    addEventListener("mousedown", (e) =>{
+
+        if (e.target.className == "optBtn" && gameStarted == false){
+
+            e.target.classList.toggle("options-pressed");
+            animGames();
+            gameStarted = true;
+            PlayGame(e.target.textContent);
+            resetText()
+        }else if(e.target.className == "optBtn" || e.target.className == "optBtn options-pressed"){
+            animGames();
+            gameStarted = false;
+            resetText();
+            
         }
-    }catch(err){
-        throw "Error: " + err.message;
+
+    })
+
+    function PlayGame(chose){
+
+        let computer = "";
+        playerChose.src = `./icons/${chose.toLowerCase()}.svg`;
+
+        randN = Math.floor(Math.random() * 3) + 1;
+        switch(randN){
+            case 1:
+                computer = "rock";
+                pcChose.src = "./icons/rock.svg";
+                break;
+            case 2:
+                computer = "paper";
+                pcChose.src = "./icons/paper.svg";
+                break;
+            case 3:
+                computer = "scissors";
+                pcChose.src = "./icons/scissors.svg";
+                break;
+
+        }
+
+        playerChose.style.visibility = "visible";
+        pcChose.style.visibility = "visible";
+
+        resultado = check(chose.toLowerCase(), computer.toLowerCase());
+
+
+        switch(resultado){
+
+            case 1:
+                resultadoTexto.innerHTML = "You won.";
+                playerPoints++;
+                document.getElementById("playerText").innerHTML = `PLAYER (${playerPoints})`;
+                break;
+
+            case 2:
+                resultadoTexto.innerHTML = "The PC won.";
+                pcPoints++;
+                document.getElementById("pcText").innerHTML = `PC (${pcPoints})`;
+                break;
+
+            case 0:
+                resultadoTexto.innerHTML = "Draw";
+                break;
+
+        }
+
+        resultadoTexto.style.visibility = "visible";
+
     }
-}
 
-function PlayGame(){
+    function check(player1, player2){
 
-    let computer = "";
-
-    randN = Math.floor(Math.random() * 3) + 1;
-    switch(randN){
-        case 1:
-            computer = "rock";
-            break;
-        case 2:
-            computer = "papers";
-            break;
-        case 3:
-            computer = "scissors";
-            break;
+        if (player1.toLowerCase() == 'rock' && player2.toLowerCase() == 'paper'){
+            return 2;
+        }else if (player1.toLowerCase() == 'scissors' && player2.toLowerCase() == 'rock'){
+            return 2;
+        }else if (player1.toLowerCase() == 'paper' && player2.toLowerCase() == 'scissors'){
+            return 2;
+        }else if (player1.toLowerCase() == player2.toLowerCase()){
+            return 0;
+        }else{
+            return 1;
+        }
     }
 
-    let player = prompt("What you want to be?: 'Rock', 'Papers', 'Scissors'");
+    function animGames(){
+        playerChose.classList.toggle("animGames");
+        pcChose.classList.toggle("animGames");
+    }
 
-    if (player == null || player == "" || (player != "rock" && player.toLowerCase() != "papers" && player.toLowerCase() != "scissors")) {
+    function resetText(){
 
-        console.log("Write something.");
-
-    }else{
-
-        console.log("PLAYER 1 (you): " + player.toUpperCase());
-        console.log("PLAYER 2 (pc): " + computer.toUpperCase());
-        console.log(check(player, computer));
-
+        if (gameStarted){
+            for (let i = 0; i < options.length; i++){
+                options[i].innerHTML = "RESET";
+            }
+        }else{
+            let arr = ['ROCK', 'PAPER', 'SCISSORS'];
+            for (let i = 0; i < options.length; i++){
+                options[i].innerHTML = arr[i];
+                options[i].className = "optBtn"
+                resultadoTexto.style.visibility = "hidden";
+            }
+        }
+        
     }
 
 }
